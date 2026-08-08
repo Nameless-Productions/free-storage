c609c905301f00ac6fcea7d7a859652ae79283d3
thenamelessdev <nameless@thenamelessdev.com>
2026-08-08 16:24:27 +0200

    feat: print out bot user

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	
+	godotenv.Load()
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		log.Fatal("No inputs provided")
+	}
+
+
+	if os.Getenv("TOKEN") == "" {
+		log.Fatal("No TOKEN in env")
+	}
+
+	session, err := discordgo.New("Bot " + os.Getenv("TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, err := session.User("@me");
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Bot: %s#%s", user.Username, user.Discriminator)
 }
