d84036e5600488f151be990758e347a199ae03bd
thenamelessdev <nameless@thenamelessdev.com>
2026-08-08 23:20:09 +0200

    feat: addMsgID func that adds a message of a uploaded file

---
+++ db.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type File struct {
+	Name string `json:"name"`
+	MessageIDs []string `json:"messageID"`
+}
+
+type DB struct {
+	Files []File `json:"files"`
+}
+
+func addMsgID(fileName, messageID string) {
+	db, err := os.ReadFile("db.json")
+	if err != nil {
+		data, _ := json.Marshal(DB{Files: []File{}})
+		os.WriteFile("db.json", []byte(data), 0644)
+		db = []byte(data)
+	}
+
+	var dbData DB
+	err = json.Unmarshal(db, &dbData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var foundIndex *int
+	for i, f := range dbData.Files {
+		if f.Name == fileName {
+			foundIndex = &i
+			break
+		}
+	}
+
+	if foundIndex == nil {
+		dbData.Files = append(dbData.Files, File{Name: fileName, MessageIDs: []string{messageID}})
+		data, _ := json.Marshal(dbData)
+		os.WriteFile("db.json", []byte(data), 0644)
+		return
+	}
+
+	data, _ := json.Marshal(dbData)
+	os.WriteFile("db.json", []byte(data), 0644)
+	dbData.Files[*foundIndex].MessageIDs = append(dbData.Files[*foundIndex].MessageIDs, messageID)
+}
